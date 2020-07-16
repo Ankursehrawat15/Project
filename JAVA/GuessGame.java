f2a235a060c0c2ae0dc5ba78c5740259156ae314
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-07-16 14:23:01 +0530

    Update GuessGame.java

--- JAVA/GuessGame.java
+++ JAVA/GuessGame.java
@@ -5,3 +5,9 @@ import java.util.Scanner;
 public class GuessGame {
 
 	public static void main(String[] args) {
+		Scanner KeyboardInput = new Scanner(System.in);
+	     
+		System.out.println("Write once Run anywhere"
+				+ "\n HINT : I am a language"
+				+ "\n Guess my name!!");
+		String secretword = "java";
